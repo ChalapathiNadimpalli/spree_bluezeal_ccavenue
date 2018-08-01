a14716325182aa3e27d2b407bee8405e828c613b
Chalapathi Raju <css_raju@live.com>
2018-08-01 15:34:25 +0530

    use ssl options

--- app/models/spree/ccavenue/payment_method.rb
+++ app/models/spree/ccavenue/payment_method.rb
@@ -5,6 +5,7 @@ module Spree
     preference :encryption_key, :string
     preference :test_mode, :boolean, default: true
     preference :mobile_schema, :string
+    preference :use_ssl, :boolean, default: false
 
     # Hardcoded, because usually merchants don't know what it should look like
     def url
